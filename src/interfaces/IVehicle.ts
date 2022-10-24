ec6cbacd232d882e3bfe4efdbf10fdda5a6cd086
Jamile <milli.santana2017@gmail.com>
2022-10-24 20:54:46 -0300

    refactor: altera o código para deixá-lo mais simples

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,11 +1,21 @@
-import { z } from 'zod';
+// import { z } from 'zod';
 
-const ivehicleSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().min(1900).max(2022),
-  color: z.string(),
-  status: z.boolean().optional(),
-  buyValue: z.number(),
-});
+// const ivehicleZodSchema = z.object({
+//   model: z.string().min(3),
+//   year: z.number().min(1900).max(2022),
+//   color: z.string(),
+//   status: z.boolean().optional(),
+//   buyValue: z.number(),
+// });
 
-export type IVehicle = z.infer<typeof ivehicleSchema>;
+// export type IVehicle = z.infer<typeof ivehicleZodSchema>;
+
+// export default ivehicleZodSchema;
+
+export interface IVehicle {
+  model: string,
+  year: number,
+  color: string,
+  status?: boolean,
+  buyValue: number,
+}
