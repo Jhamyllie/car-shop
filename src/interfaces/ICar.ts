0aacf756fbc80f5cc664a4c4b6ba1aa183abf135
Jamile <milli.santana2017@gmail.com>
2022-10-30 19:17:17 -0300

    refactor: implementa interface ICar a partir de IVehicle

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,8 +1,19 @@
-import { IVehicle } from './IVehicle';
+// import { IVehicle } from './IVehicle';
 
-export interface ICar extends IVehicle {
-  doorsQty: number
-  seatsQty: number,
-}
+// export interface ICar extends IVehicle {
+//   doorsQty: number
+//   seatsQty: number,
+// }
 
 // não dá, simplesmente não consigo extender e usar o zod ao mesmo tempo. Eu não aṕrendi nada
+
+import { z } from 'zod';
+import ivehicleZodSchema from './IVehicle';
+
+const CarZodSchema = ivehicleZodSchema.extend({
+  doorsQty: z.number().min(2).max(3),
+  seatsQty: z.number().min(2).max(7),
+});
+
+type ICar = z.infer<typeof CarZodSchema>;
+export { CarZodSchema, ICar };
