d1f5ad6813f2dd3f9d6cd371cd11421bd85fb821
Jamile <milli.santana2017@gmail.com>
2022-11-01 18:18:50 -0300

    refactor: Refatora o jeito de exportar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -8,11 +8,11 @@
 // não dá, simplesmente não consigo extender e usar o zod ao mesmo tempo. Eu não aṕrendi nada
 
 import { z } from 'zod';
-import ivehicleZodSchema from './IVehicle';
+import { ivehicleZodSchema } from './IVehicle';
 
 const CarZodSchema = ivehicleZodSchema.extend({
-  doorsQty: z.number().min(2).max(3),
-  seatsQty: z.number().min(2).max(7),
+  doorsQty: z.number().int().min(2).max(4),
+  seatsQty: z.number().int().min(2).max(7),
 });
 
 type ICar = z.infer<typeof CarZodSchema>;
