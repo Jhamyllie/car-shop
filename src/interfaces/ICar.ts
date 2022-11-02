8999b02ab68948a68c5488531e11236735c54e19
Jamile <milli.santana2017@gmail.com>
2022-11-02 10:18:11 -0300

    fix: alteração no arquivo

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,12 +1,3 @@
-// import { IVehicle } from './IVehicle';
-
-// export interface ICar extends IVehicle {
-//   doorsQty: number
-//   seatsQty: number,
-// }
-
-// não dá, simplesmente não consigo extender e usar o zod ao mesmo tempo. Eu não aṕrendi nada
-
 import { z } from 'zod';
 import { ivehicleZodSchema } from './IVehicle';
 
