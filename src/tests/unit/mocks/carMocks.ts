d93ced2ee39445a8b1d3a18d80e8798ae2d54e9d
Jamile <milli.santana2017@gmail.com>
2022-11-03 21:20:30 -0300

    fix: muda os arquivos de testes para tentar cobrir 15% do model

--- src/tests/unit/mocks/carMocks.ts
+++ src/tests/unit/mocks/carMocks.ts
@@ -1,48 +1,73 @@
-import { ICar } from "../../../interfaces/ICar";
+// import { ICar } from "../../../interfaces/ICar";
+
+// const carMock: ICar = {
+//     buyValue: 50000,
+//     color: 'green',
+//     doorsQty: 4,
+//     model: 'fusca',
+//     seatsQty: 5,
+//     year: 1980,
+// };
+
+// const carMockId: ICar & { _id: string } = {
+//     _id: 'd524dwsd6sa4d4d5sa',
+//     buyValue: 50000,
+//     color: 'green',
+//     doorsQty: 4,
+//     model: 'fusca',
+//     seatsQty: 5,
+//     year: 1980,
+//     status: true
+// }
+
+// const listCar: [
+//     {
+//         _id: 'd524dwsd6sa4d4d5sa',
+//         buyValue: 50000,
+//         color: 'green',
+//         doorsQty: 4,
+//         model: 'fusca',
+//         seatsQty: 5,
+//         year: 1980,
+//         status: true
+//     },
+//     {
+//         _id: 'd524dwsd6sa4d4d5sa',
+//         buyValue: 50000,
+//         color: 'black',
+//         doorsQty: 4,
+//         model: 'impala',
+//         seatsQty: 5,
+//         year: 1980,
+//         status: true
+//     }
+// ]
+
+// const carsList = [ carMockId, carMockId, carMockId ];
+
+// export {carMock, carMockId, carsList, listCar };
+
+import { ICar } from "../../../interfaces/ICar"; 
 
 const carMock: ICar = {
-    buyValue: 50000,
-    color: 'green',
-    doorsQty: 4,
-    model: 'fusca',
-    seatsQty: 5,
-    year: 1980,
+  model: "Gol GTI",
+  year: 1988,
+  color: "white",
+  status: true,
+  buyValue: 35000,
+  doorsQty: 2,
+  seatsQty: 2,
 };
 
-const carMockId: ICar & { _id: string } = {
-    _id: 'd524dwsd6sa4d4d5sa',
-    buyValue: 50000,
-    color: 'green',
-    doorsQty: 4,
-    model: 'fusca',
-    seatsQty: 5,
-    year: 1980,
-    status: true
-}
+const carMockWithId: ICar & { _id: string } = {
+  _id: '6359e47b94dc48d1e10c2f20',
+  model: "Gol GTI",
+  year: 1988,
+  color: "white",
+  status: true,
+  buyValue: 35000,
+  doorsQty: 2,
+  seatsQty: 2,
+};
 
-const listCar: [
-    {
-        _id: 'd524dwsd6sa4d4d5sa',
-        buyValue: 50000,
-        color: 'green',
-        doorsQty: 4,
-        model: 'fusca',
-        seatsQty: 5,
-        year: 1980,
-        status: true
-    },
-    {
-        _id: 'd524dwsd6sa4d4d5sa',
-        buyValue: 50000,
-        color: 'black',
-        doorsQty: 4,
-        model: 'impala',
-        seatsQty: 5,
-        year: 1980,
-        status: true
-    }
-]
-
-const carsList = [ carMockId, carMockId, carMockId ];
-
-export {carMock, carMockId, carsList, listCar };
+export { carMock, carMockWithId };
--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -1,36 +1,25 @@
-import { expect } from 'chai';
-import sinon from 'sinon';
-import { Model } from 'mongoose';
-import { carMock, carMockId, carsList } from '../mocks/carMocks';
+import * as sinon from 'sinon';
+import chai from 'chai';
 import Cars from '../../../models/cars';
+import { Model } from 'mongoose';
+import { carMock, carMockWithId  } from '../mocks/carMocks'; 
+
+const { expect } = chai;
 
 describe('Car Model', () => {
   const carModel = new Cars();
 
-  before(() => {
-    sinon.stub(Model, 'create').resolves(carMockId),
-    sinon.stub(Model, 'find').resolves(carsList),
-    sinon.stub(Model, 'findOne').resolves(carMockId),
-    sinon.stub(Model, 'findByIdAndUpdate').resolves(carMockId),
-    sinon.stub(Model, 'findByIdAndDelete').resolves(carMockId)
+  before(async () => {  
+    sinon.stub(Model, 'create').resolves(carMockWithId);
+  });
+  after(()=>{
+    sinon.restore();
   });
 
-  after(() => { 
-    sinon.restore() 
-  });
-
-  describe('cria um carro', () => {
-    it('successfully created', async () => {
-      const newCar = await carModel.create(carMock);
-      expect(newCar).to.be.deep.equal(carMockId);
-    });
-  });
-
-
-  describe('lista todos os carros', () => {
-    it ('encontrado com sucesso', async () => {
-    const car = await carModel.read();
-    expect(car).to.be.deep.equal(carsList);
+  describe('Cria um carro', () => {
+    it('Se criado com sucesso', async () => {
+      const result = await carModel.create(carMock);
+      expect(result).to.be.deep.equal(carMockWithId);
     });
   });
 });
