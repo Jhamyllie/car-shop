5017eabff8cf83f0a2c6d3a4446f867f06ed3ff1
Jamile <milli.santana2017@gmail.com>
2022-11-07 20:38:29 -0300

    fix: implementa mais testes à camada de model

--- src/tests/unit/mocks/carMocks.ts
+++ src/tests/unit/mocks/carMocks.ts
@@ -1,52 +1,3 @@
-// import { ICar } from "../../../interfaces/ICar";
-
-// const carMock: ICar = {
-//     buyValue: 50000,
-//     color: 'green',
-//     doorsQty: 4,
-//     model: 'fusca',
-//     seatsQty: 5,
-//     year: 1980,
-// };
-
-// const carMockId: ICar & { _id: string } = {
-//     _id: 'd524dwsd6sa4d4d5sa',
-//     buyValue: 50000,
-//     color: 'green',
-//     doorsQty: 4,
-//     model: 'fusca',
-//     seatsQty: 5,
-//     year: 1980,
-//     status: true
-// }
-
-// const listCar: [
-//     {
-//         _id: 'd524dwsd6sa4d4d5sa',
-//         buyValue: 50000,
-//         color: 'green',
-//         doorsQty: 4,
-//         model: 'fusca',
-//         seatsQty: 5,
-//         year: 1980,
-//         status: true
-//     },
-//     {
-//         _id: 'd524dwsd6sa4d4d5sa',
-//         buyValue: 50000,
-//         color: 'black',
-//         doorsQty: 4,
-//         model: 'impala',
-//         seatsQty: 5,
-//         year: 1980,
-//         status: true
-//     }
-// ]
-
-// const carsList = [ carMockId, carMockId, carMockId ];
-
-// export {carMock, carMockId, carsList, listCar };
-
 import { ICar } from "../../../interfaces/ICar"; 
 
 const carMock: ICar = {
@@ -70,4 +21,6 @@ const carMockWithId: ICar & { _id: string } = {
   seatsQty: 2,
 };
 
-export { carMock, carMockWithId };
+const carListMock: ICar[] = [carMockWithId];
+
+export { carMock, carMockWithId, carListMock };
--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 import Cars from '../../../models/cars';
 import { Model } from 'mongoose';
-import { carMock, carMockWithId  } from '../mocks/carMocks'; 
+import { carMock, carMockWithId, carListMock  } from '../mocks/carMocks'; 
 
 const { expect } = chai;
 
@@ -11,6 +11,12 @@ describe('Car Model', () => {
 
   before(async () => {  
     sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'find').resolves(carListMock);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
+    sinon.stub(Model, 'findOneAndUpdate').resolves(carMockWithId);
+    sinon.stub(Model, 'findByIdAndDelete').resolves(carMockWithId);
+
+
   });
   after(()=>{
     sinon.restore();
@@ -20,6 +26,34 @@ describe('Car Model', () => {
     it('Se criado com sucesso', async () => {
       const result = await carModel.create(carMock);
       expect(result).to.be.deep.equal(carMockWithId);
+    });
+  });
+
+  describe('Lista os carros', () => {
+    it('listado com sucesso', async() => {
+      const result = await carModel.read();
+      expect(result).to.be.equal(carListMock);
+    });
+  });
+
+  describe('retorna um carro pelo id', () => {
+    it('Em caso de sucesso', async () => {
+      const result = await carModel.readOne(carMockWithId._id);
+      expect(result).to.be.equal(carMockWithId);
+    });
+  });
+
+  describe('É possível atualizar um carro', () => {
+    it('Em caso de sucesso', async () => {
+      const result = await carModel.update(carMockWithId._id, carMock);
+      expect(result).to.be.equal(carMockWithId);
+    });
+  });
+
+  describe('É possível deletar um carro', () => {
+    it('Deletado com sucesso', async () => {
+      const result = await carModel.delete(carMockWithId._id);
+      expect(result).to.be.equal(carMockWithId);
     });
   });
 });
