f9f20439c105fae4962f3acacb0f1022158d4a51
Jamile <milli.santana2017@gmail.com>
2022-11-07 19:02:24 -0300

    fix: adiciona mais testes à camada de model

---
+++ src/tests/unit/mocks/motorcycleMock.ts
@@ -0,0 +1,12 @@
+// import { IMotorcycle } from "../../../interfaces/IMotorcycle";
+
+// const motorcycleMock: IMotorcycle = {
+//     model: "Gol GTI",
+//     year: 1988,
+//     color: "white",
+//     status: true,
+//     buyValue: 35000,
+//     category: 
+// };
+
+// export { motorcycleMock };
